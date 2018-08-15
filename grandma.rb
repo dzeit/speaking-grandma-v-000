11450cd117ea888e4ecb0eed445b1baa503bc9b9
David Zeit <dzeit216@gmail.com>
2018-08-15 18:10:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  else phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
     end
 end
 # Whatever you say to grandma, she should respond with
